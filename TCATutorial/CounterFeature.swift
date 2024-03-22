f953b32e706f0753ab725f15491688d9a57db649
hanhqvu <hanhqvu@users.noreply.github.com>
2024-03-22 22:39:01 +0900

    init

---
+++ TCATutorial/CounterFeature.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCATutorial
+//
+//  Created by Hanh Vu on 2024/03/22.
+//
+
+import SwiftUI
+
+struct CounterFeature: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    CounterFeature()
+}
---
+++ TCATutorial/TCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialApp.swift
+//  TCATutorial
+//
+//  Created by Hanh Vu on 2024/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CounterFeature()
+        }
+    }
+}
